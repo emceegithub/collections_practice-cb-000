0ec85a509779778370fa1ff0d0e62de34cc3733f
Mark Cruz <markchriscruz@yahoo.com>
2018-03-07 21:42:52 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,3 +18,10 @@ def sort_array_char_count(array)
   end
 end
 
+def swap_elements(array)
+  second_element = array[1]
+  third_element = array[2]
+  array[1] = third_element
+  array[2] = second_element
+  return array
+end
