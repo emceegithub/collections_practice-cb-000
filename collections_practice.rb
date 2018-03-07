ebab79fbf3ec029ea137606b6852835abca2446b
Mark Cruz <markchriscruz@yahoo.com>
2018-03-07 22:34:56 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,80 @@
+def sort_array_asc(array)
+  return array.sort
+end
+
+def sort_array_desc(array)
+  return array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  second_element = array[1]
+  third_element = array[2]
+  array[1] = third_element
+  array[2] = second_element
+  return array
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+def kesha_maker(array)
+  collector = []
+  array.each do |element|
+    collector << element[0..1] + "$" + element[3..element.length-1]
+  end
+  return collector
+end
+
+def find_a(array)
+  collector = []
+  array.each do |element|
+    if element.start_with?("a")
+      collector << element
+    else
+      # do nothing
+    end
+  end
+  return collector
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |element|
+    sum = sum + element
+  end 
+  return sum
+end
+
+def add_s(array)
+  collector = []
+  array.each_with_index do |element, index|
+    if index == 1
+      collector << element
+    else
+      collector << element + "s"
+    end
+  end
+  return collector
+end
+
+
+
+
+
+
+
+
 
